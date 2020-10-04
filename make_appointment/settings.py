87e4303657d210ac8e9efa25bf6a60f0812b3a81
Dilshod Bakhtiyorov <bakhtiyorovdilshod440@gamil.com>
2020-10-04 15:01:06 +0500

    ready

--- make_appointment/settings.py
+++ make_appointment/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-3f==d-a#_wi&rk$^)#xd_thwjlixb8*sr4u*fb=&k-+-lnabt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
